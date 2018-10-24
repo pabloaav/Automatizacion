8a16dcecb673ee8bb95340f019baff9dbbfefff8
pabloaav <pablovicentin84@gmail.com>
2018-10-24 12:40:57 -0300

    Primeros pasos con Test Selenium

--- Principal.py
+++ Principal.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 driver = webdriver.Chrome('chromedriver.exe')
 """
 Loads a web page in the current browser session.
@@ -6,6 +7,9 @@ El metodo get de web driver implementa el metodo GET HTTP,
 obteniendo o trayendo la url pasada como parametro
 """
 driver.get('http://newtours.demoaut.com/')
+
+#damos tiempo a que cargue la pagina
+time.sleep(3)
 
 """
 Por medio del objeto driver que manipula la API de comunicacion con el browser,
@@ -22,3 +26,7 @@ indicandole acciones, a traves de metodos de prueba
 user_box.send_keys("test")
 pass_box.send_keys("test")
 submit_button.click()
+# Tiempo de carga
+time.sleep(3)
+# Cerramos el browser
+driver.quit()
