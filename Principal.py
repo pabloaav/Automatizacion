1a9a5d9288f6448f925d49b84e9ffa9e0bb186e4
pabloaav <pablovicentin84@gmail.com>
2018-10-24 12:29:46 -0300

    Adicionando los primeros elementos de prueba

--- Principal.py
+++ Principal.py
@@ -6,3 +6,19 @@ El metodo get de web driver implementa el metodo GET HTTP,
 obteniendo o trayendo la url pasada como parametro
 """
 driver.get('http://newtours.demoaut.com/')
+
+"""
+Por medio del objeto driver que manipula la API de comunicacion con el browser,
+hacemos uso de metodos predefinidos en el webdriver de Selenium para Chrome.
+"""
+user_box = driver.find_element_by_name("userName")
+pass_box = driver.find_element_by_name("password")
+submit_button = driver.find_element_by_name("login")
+
+"""
+Una vez cargados los elementos, podemos interactuar con ellos, por medio de las variables definidas
+indicandole acciones, a traves de metodos de prueba 
+"""
+user_box.send_keys("test")
+pass_box.send_keys("test")
+submit_button.click()
