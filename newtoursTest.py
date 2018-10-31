67900baaa5233544e49b41a5ec83a408da8ec2e1
pabloaav <pablovicentin84@gmail.com>
2018-10-31 12:12:31 -0300

    Omision de casos de prueba (skip test)

--- newtoursTest.py
+++ newtoursTest.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from Pages.pageIndex import *
 from Pages.pageRegister import *
 import xmlrunner
-import  HtmlTestRunner
+import HtmlTestRunner
 import unittest
+import sys
 """
 Se define una clase que hereda de unittes.TestCase
 """
-class NewTours (unittest.TestCase):
+
+
+class NewTours(unittest.TestCase):
     url = 'http://newtours.demoaut.com/'
 
     """
@@ -15,15 +18,20 @@ class NewTours (unittest.TestCase):
     inciailes para los casos de prueba, y que resultan repetitivas y generales.
     Método para configurar el dispositivo de prueba antes de ejercitarlo.
     """
+
     def setUp(self):
-        self.driver = webdriver.Chrome('chromedriver.exe')
+        # self.driver = webdriver.Chrome('chromedriver.exe')
+        # Hacemos uso de PhantomJS para acelerar las pruebas en cuanto a tiempo total de ejecucion
+        self.driver = webdriver.PhantomJS()
         self.driver.get(self.url)
         # Instanciar objeto tipo PageIndex y un objeto PageRegister
         self.index_page = PageIndex(self.driver)
         self.register_page = PageRegister(self.driver)
+
     """
     Seccion de Test Cases: se colocan aqui los casos de prueba
     """
+
     def test_register(self):
         self.index_page.clic_register()
         self.register_page.country_select_index(4)
@@ -31,26 +39,28 @@ class NewTours (unittest.TestCase):
         self.register_page.country_select_visibleText("CONGO")
         self.register_page.verify_country("CONGO")
 
-
+    # @unittest.skip("Omitir por ahora")
+    # @unittest.skipIf(5 > 4, "Si la expresion logica resulta verdadera, se omite el test")
+    @unittest.skipUnless(sys.platform.startswith('linux'),"A menos que sea Linux")
     def test_login(self):
-        self.index_page.login("test","test")
+        self.index_page.login("test", "test")
         link_registration_form = self.driver.find_element_by_link_text("registration form")
         self.assertEqual(link_registration_form.text, "registration form")
 
     def test_login_by_tabs(self):
-        self.index_page.login_by_tab("test","test")
+        self.index_page.login_by_tab("test", "test")
 
     """
     Finalizar el unti Test con tearDown()
     """
+
     def tearDown(self):
         self.driver.close()
         self.driver.quit()
 
+
 if __name__ == '__main__':
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='example_dir'))
-
-
 
     # """
     # Generar reporte XML de los Test corridos
